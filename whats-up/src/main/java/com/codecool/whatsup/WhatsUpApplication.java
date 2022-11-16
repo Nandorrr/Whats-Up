1906f76f5e2fa9fb8f937811c3b22dc6c480d9d5
Nandorrr <87696275+Nandorrr@users.noreply.github.com>
2022-11-16 16:45:49 +0100

    add project starter

---
+++ whats-up/src/main/java/com/codecool/whatsup/WhatsUpApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.whatsup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsUpApplication.class, args);
+    }
+
+}

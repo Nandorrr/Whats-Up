7bb40c5ab0ca295775cefba86bbda61ecee37553
Nandorrr <87696275+Nandorrr@users.noreply.github.com>
2022-11-19 23:54:44 +0100

    add basic logic to App.tsx

--- whats-up/frontend/src/App.tsx
+++ whats-up/frontend/src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Login from "./Components/Login/Login";
+import ChatRoom from "./Components/ChatRoom/ChatRoom";
 
 function App() {
+  const [userName, setUserName] = useState("");
+  const [room, setRoom] = useState("");
+
   return (
-    <div className="App">
-    </div>
+      <div>
+        {userName ==="" ? (
+            <Login setUserName={setUserName} />
+        ) :
+        <ChatRoom/>
+        }
+      </div>
   );
 }
 

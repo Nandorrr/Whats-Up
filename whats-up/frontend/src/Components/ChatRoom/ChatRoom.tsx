b87eb8c4b5708aa7f31d78215f8680c6d8cc0724
Nandorrr <87696275+Nandorrr@users.noreply.github.com>
2022-11-28 11:01:14 +0100

    removed hardcoded values from ChatRoom.tsx and add base logic

--- whats-up/frontend/src/Components/ChatRoom/ChatRoom.tsx
+++ whats-up/frontend/src/Components/ChatRoom/ChatRoom.tsx
@@ -1,42 +1,88 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {useSocket} from "../../CustomHooks/useSocket";
 import './ChatRoom.css';
+import Contact from "../Contact/Contact";
+import Message from "../Message/Message";
 
 interface Props{
     username: string;
+    room: string
+}
+interface userData {
+        username: string,
+        receiver: string,
+        message: string,
+        status:string
+
 }
 
-function ChatRoom({username}: Props){
-    let  {isConnected, socketResponse, sendData } = useSocket(username);
+function ChatRoom({username, room}: Props){
+    let  {isConnected, socketResponse, sendData } = useSocket(username, room);
+    const [userData, setUserData] = useState({
+        username: username,
+        receiver: '',
+        message: '',
+        status:'MESSAGE'
+    });
+    let [message, setMessage] = useState<string>("");
+    let [messageList, setMessageList] = useState<userData[]>([]);
+
+
+    let handleInput = (e: React.FormEvent<HTMLInputElement>) =>{
+        setMessage(e.currentTarget.value);
+        setUserData({...userData, message: message});
+    }
+
+    let addMessageToList = (payload : any)  => {
+        if(userData.message.trim() !=="") {
+            messageList.push(payload);
+            setMessageList(messageList)
+            console.log(messageList)
+        }
+    }
+
+    let sendMessage = (e: React.FormEvent<HTMLFormElement>) =>{
+        e.preventDefault();
+        if (message.trim()!==""){
+            console.log(userData)
+            sendData({message: userData.message})
+            addMessageToList(userData);
+            setMessage("");
+        }
+
+    }
+
+    useEffect(() => {
+        console.log("Socket Response: ", socketResponse);
+        addMessageToList(socketResponse);
+    }, [socketResponse]);
+
+    useEffect(() => {
+        console.log("MessageList: ", messageList);
+
+    }, [messageList]);
+
+
 
     return(
         <div className="main-container">
            <div className="contacts-holder">
-               <div className="contact">Dave</div>
-               <div className="contact">Jon</div>
-               <div className="contact">Alex</div>
-               <div className="contact">Crissy</div>
+                   <Contact username={username} key={1}/>
            </div>
             <div className="right-container">
+                {messageList.map((item : userData) =>
+                    <Message username={item.username} message={item.message}/>
+                )}
                 <div className="message-container">
-                    <div className="message">
-                        <div  className="name">Dave</div>
-                       <div className="content"> Hello</div>
-                    </div>
-                    <div className="message">
-                        <div  className="name">Nick</div>
-                        <div  className="content"> Hello</div>
-                    </div>
-                    <div className="message mine">
-                        <div className="name">Missy</div>
-                        <div className="content"> Hello</div>
-                    </div>
-                    <div className="message mine">
-                            <div className="name">Missy</div>
-                            <div className="content"> How you all doin?</div>
-                    </div>
                     <div className="input-holder">
-                        <input type="text" placeholder="Type...."/>
+                        <form onSubmit={(e)=>{sendMessage(e)}}>
+                            <input type="text"
+                                   placeholder="Type...."
+                                   value={message}
+                                   onChange={handleInput}
+                            />
+                            <button type="submit">Send</button>
+                        </form>
                     </div>
                 </div>
             </div>

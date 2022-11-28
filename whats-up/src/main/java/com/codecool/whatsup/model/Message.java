682f116e95b8233f64af455ab64273ef2f9d1705
Nandorrr <87696275+Nandorrr@users.noreply.github.com>
2022-11-28 16:09:31 +0100

    modified receiver to room in Message.java and add Entity and id annotations

--- whats-up/src/main/java/com/codecool/whatsup/model/Message.java
+++ whats-up/src/main/java/com/codecool/whatsup/model/Message.java
@@ -5,13 +5,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
 public class Message {
     private String sender;
-    private String receiver;
+    private String room;
     private String message;
     private Status status;
+    @Id
+    private Long id;
+
+
 }

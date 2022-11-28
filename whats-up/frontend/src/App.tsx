b4b64a5ee2dc81bce63bb9088e26b866164a1849
Nandorrr <87696275+Nandorrr@users.noreply.github.com>
2022-11-28 11:01:50 +0100

    add type to username in App.tsx and create a default room

--- whats-up/frontend/src/App.tsx
+++ whats-up/frontend/src/App.tsx
@@ -4,14 +4,15 @@ import Login from "./Components/Login/Login";
 import ChatRoom from "./Components/ChatRoom/ChatRoom";
 
 function App() {
-  const [userName, setUserName] = useState("");
+  const [userName, setUserName] = useState<string>("");
+  const room: string = "default";
 
   return (
       <div>
         {userName ==="" ? (
             <Login setUserName={setUserName} />
         ) : (
-             <ChatRoom username={userName}/>
+             <ChatRoom username={userName} room={room}/>
         )
         }
       </div>
